1cd9307aa05ee6957c0e9808f2d7b75838d99af2
Cycleurope <54021538+Cycleurope@users.noreply.github.com>
2020-02-20 10:54:13 +0100

    Commit

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -2,85 +2,12 @@
 @section('content')
 <div id="app" class="py-5">
     @include('partials/notifications-panel')
-    <div class="container-fluid px-5">
-        <div class="row">
-            <div class="col-12">
-                <h1>Profil</h1>
-            </div>
-            <div class="col-8 mb-5">
-                <h3>Mes informations</h3>
-                <table class="table table-sm">
-                    <thead>
-                        <tr>
-                            <th>&nbsp;</th>
-                            <th>&nbsp;</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td><span class="font-weight-bold">Identifiant</span></td>
-                            <td>{{ Auth::user()->username }}</td>
-                        </tr>
-                        <tr>
-                            <td><span class="font-weight-bold">Nom</span></td>
-                            <td>{{ Auth::user()->name }}</td>
-                        </tr>
-                        <tr>
-                            <td><span class="font-weight-bold">Adresse - Ligne 1</span></td>
-                            <td>{{ Auth::user()->address1 }}</td>
-                        </tr>
-                        <tr>
-                            <td><span class="font-weight-bold">Adresse - Ligne 2</span></td>
-                            <td>{{ Auth::user()->address2 }}</td>
-                        </tr>
-                        <tr>
-                            <td><span class="font-weight-bold">Code Postal</span></td>
-                            <td>{{ Auth::user()->postalcode }}</td>
-                        </tr>
-                        <tr>
-                            <td><span class="font-weight-bold">Ville</span></td>
-                            <td>{{ Auth::user()->city }}</td>
-                        </tr>
-                        <tr>
-                            <td><span class="font-weight-bold">Téléphone</span></td>
-                            <td>{{ Auth::user()->phone }}</td>
-                        </tr>
-                        <tr>
-                            <td><span class="font-weight-bold">E-mail</span></td>
-                            <td>{{ Auth::user()->email }}</td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-            <div class="col-4 mb-5">
-                <h3>Mes assortiments</h3>
-                <table class="table table-sm">
-                    <thead>
-                        <tr>
-                            <th>Catalogue</th>
-                            <th class="text-right">Jusqu'au</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach(Auth::user()->assortments as $a)
-                        <tr>
-                            <td>
-                                @component('components.assortment')
-                                    @slot('name')
-                                    {{ $a->ocascd }}
-                                    @endslot
-                                @endcomponent
-                            </td>
-                            <td class="text-right">{{ $a->octdat }}</td>
-                        </tr>
-                        @endforeach
-                    </tbody>
-                </table>
-            </div>
-            <div class="col-12">
-                <a href="{{ route('password.index') }}" class="btn flatbutton">Modifier mon mot de passe</a>
-            </div>
-        </div>
-    </div>
+    @if(Auth::user()->role == 'admin')
+        @include('profile.admin')
+    @elseif(Auth::user()->role == 'dealer')
+        @include('profile.dealer')
+    @elseif(Auth::user()->role == 'sales')
+        @include('profile.sales')
+    @endif
 </div>
 @endsection
