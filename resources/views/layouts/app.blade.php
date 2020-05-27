be7c9c3421684c2c63d66688ad69aab93978d55d
Cycleurope <54021538+Cycleurope@users.noreply.github.com>
2020-05-27 12:24:39 +0200

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -52,7 +52,6 @@
         @yield('content')
         </div>
     </div>
-    <script src="//{{ Request::getHost() }}:6001/socket.io/socket.io.js"></script>
     <script src="{{ asset('js/app.js') }}"></script>
 </body>
 </html>
