a754ac06337e2ec0f7731406379da1732d8abe89
Cycleurope <web@cycleurope.fr>
2020-05-28 15:58:38 +0200

    Commit

--- public/js/app.js
+++ public/js/app.js
@@ -82364,8 +82364,8 @@ if (document.getElementById('products-finder')) {
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/rlomvin/webapps/rstock2020/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/rlomvin/webapps/rstock2020/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Users/vincentlombard/Webapps/rstock2020/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Users/vincentlombard/Webapps/rstock2020/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
