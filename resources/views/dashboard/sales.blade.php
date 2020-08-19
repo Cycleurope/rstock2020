7c3a2b8eb10052c962f8adff084ff64007cc20d7
Cycleurope <54021538+Cycleurope@users.noreply.github.com>
2020-08-19 08:49:24 +0200

    Update sales.blade.php

--- resources/views/dashboard/sales.blade.php
+++ resources/views/dashboard/sales.blade.php
@@ -8,7 +8,8 @@
             </div>
         </div>
         <div class="col-12">
-            <a href="{{ route('export.dealers') }}" class="btn btn-success text-white">Exporter la liste des comptes</a>
+            <a href="{{ route('export.stock.all') }}" class="btn btn-primary text-white">Exporter tout le stock</a>
+                    <a href="{{ route('export.dealers') }}" class="btn btn-success text-white">Exporter la liste des comptes</a>
         </div>
     </div>
 @endsection
